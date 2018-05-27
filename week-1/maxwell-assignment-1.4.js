8072332171fd9369baeab3ef6aa92b47c69198d4
Leon Maxwell <wxmaxx@gmail.com>
2018-05-27 06:12:10 -0500

    Adding assignment 1.4

---
+++ week-1/maxwell-assignment-1.4.js
@@ -0,0 +1,41 @@
+/*
+====================================================
+Title: Assignment 1.4
+Author: Professor Krasso
+Date: 27 May 2018
+Modified By: Leon Maxwell
+Description: Primitive types assignment 1.4.
+====================================================
+*/
+
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = 'Leon';
+var lastName = 'Maxwell';
+var age = 49;
+var street = '17209 Samantha Rd';
+var city = 'Gretna';
+var state = 'Nebraska';
+var zipCode = '68028';
+
+// output 
+console.log(firstName + ' ' + lastName + '\n');
+console.log(age + '\n');
+console.log(street + '\n');
+console.log(city +',' + ' ' + state + ' ' + zipCode);
+
+
+// end program 
