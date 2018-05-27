4b5dfd3d8d23d58a87a78f77442623105ba27889
Leon Maxwell <wxmaxx@gmail.com>
2018-05-27 06:24:55 -0500

    Adding exercise 1.5 and renamed hello world to 1.3

---
+++ week-1/maxwell-assignment-1.5.js
@@ -0,0 +1,33 @@
+/*
+====================================================
+Title: Assignment 1.5
+Author: Professor Krasso
+Date: 27 May 2018
+Modified By: Leon Maxwell
+Description: Types, values, and variables 1.5.
+====================================================
+*/
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "Leon";
+var myLastName = "Maxwell";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+
+
+// end of program 
