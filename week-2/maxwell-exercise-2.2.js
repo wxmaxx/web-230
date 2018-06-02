40699ce0a60dfdc316cbdb9ccfe6673860288482
Leon Maxwell <wxmaxx@gmail.com>
2018-06-02 00:42:01 -0500

    Adding week 2, exercise 2.2

---
+++ week-2/maxwell-exercise-2.2.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   25 June 2017
+; Modified By: <Leon Maxwell>
+; Description: Displays a formatted header
+;===========================================
+*/ 
+
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Leon", "Maxwell", "Exercise 2.2")); 
+console.log("\n");
