cf6d8a950e764c70970c1b5d0d2e2d75a0048040
Leon <wxmaxx@gmail.com>
2018-06-20 06:04:41 -0500

    Added some research data at the end

--- week-5/maxwell-exercise-5.4.js
+++ week-5/maxwell-exercise-5.4.js
@@ -120,6 +120,7 @@ console.log("");
 
 
 // output the composer by genre
+
 console.log("-- COMPOSER BY GENRE --");
 console.log("");
 console.log(composersByGenre);
@@ -127,3 +128,42 @@ console.log(composersByGenre);
 
 
 // end program
+
+// information for looping throught he code follows
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+// const obj = { foo: 'bar', baz: 42 };
+// console.log(Object.entries(obj)); // [ ['foo', 'bar'], ['baz', 42] ]
+
+// // array like object
+// const obj = { 0: 'a', 1: 'b', 2: 'c' };
+// console.log(Object.entries(obj)); // [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ]
+
+// // array like object with random key ordering
+// const anObj = { 100: 'a', 2: 'b', 7: 'c' };
+// console.log(Object.entries(anObj)); // [ ['2', 'b'], ['7', 'c'], ['100', 'a'] ]
+
+// // getFoo is property which isn't enumerable
+// const myObj = Object.create({}, { getFoo: { value() { return this.foo; } } });
+// myObj.foo = 'bar';
+// console.log(Object.entries(myObj)); // [ ['foo', 'bar'] ]
+
+// // non-object argument will be coerced to an object
+// console.log(Object.entries('foo')); // [ ['0', 'f'], ['1', 'o'], ['2', 'o'] ]
+
+// // returns an empty array for any primitive type, since primitives have no own properties
+// console.log(Object.entries(100)); // [ ]
+
+// // iterate through key-value gracefully
+// const obj = { a: 5, b: 7, c: 9 };
+// for (const [key, value] of Object.entries(obj)) {
+//   console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+// }
+
+// // Or, using array extras
+// Object.entries(obj).forEach(([key, value]) => {
+// console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+// });
+
+
+
