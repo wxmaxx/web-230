fcc1404b85f1fbbf1feae1ca78757966c93c3b71
Leon Maxwell <wxmaxx@gmail.com>
2018-06-24 23:49:16 -0500

    Week 5 still not working

--- week-5/maxwell-exercise-5.4.js
+++ week-5/maxwell-exercise-5.4.js
@@ -4,7 +4,7 @@
 ; Author: Professor Krasso 
 ; Date:   25 June 2017
 ; Modified By: <Leon Maxwell>
-; Modified date: 19 June 2018
+; Modified date: 24 June 2018
 ; Description: Filtering/reducing complex data structures
 ;===========================================
 */
@@ -47,8 +47,8 @@ Expected output:
  Genre: Classical
  Composer: Schubert
  */
-// Adding header information
 
+// Adding header information
 var header = require('../header.js');
 
 console.log(header.display("Leon", "Maxwell", "Exercise 5.4")); 
@@ -91,6 +91,25 @@ console.log("\n");
     }
 ];
 
+
+var comnposersByRating = famousComposers.map(function(composer) {
+    const result = composer.filter(x, x);
+
+});
+
+var composersByGenre = famousComposers.map(function(composer) {
+
+});
+
+console.log("-- COMPOSER BY RATING --" + "\n");
+console.log(result);
+
+
+console.log("-- COMPOSER BY GENRE --");
+
+
+
+// end program
 // famousComposers.forEach(function(composer){
 //     console.log(
 //         "Last Name: " + composer.lastName + ", Genre: " + composer.genre + ", Rating: " + composer.rating
@@ -99,71 +118,69 @@ console.log("\n");
 
 
 
-var comnposersByRating = famousComposers.map(function(composer) {
-    famousComposers.forEach(function(composer){
-        console.log(
-            "Last Name: " + composer.lastName + "\n" + ", Rating: " + composer.rating + "\n"
-        );
-    })
-    });
-
-// var composersByGenre = famousComposers.map(function(composer) {
-
-// });
-
-// output the composer by rating
-console.log("-- COMPOSER BY RATING --");
-console.log("");
-// console.log(famousComposers[2]);
-console.log("Last Name: " + composer.lastName + "\n" + ", Rating: " + composer.rating + "\n");
-console.log("");
-
-
-// output the composer by genre
-
-console.log("-- COMPOSER BY GENRE --");
-console.log("");
-console.log(composersByGenre);
+// var comnposersByRating = obj.map(function(composer) {
+//     for(const [key, value] of Object.entries(obj)); {
+//         console.log('${key} ${value}');
+//     };
 
 
 
-// end program
 
-// information for looping throught he code follows
-// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+// // output the composer by rating
+// console.log("-- COMPOSER BY RATING --");
+// console.log(Object.entries(obj));
+// // console.log("Last Name: " + composer.lastName + "\n" + ", Rating: " + composer.rating + "\n");
 
-// const obj = { foo: 'bar', baz: 42 };
-// console.log(Object.entries(obj)); // [ ['foo', 'bar'], ['baz', 42] ]
+// // // console.log(famousComposers[2]);
+// // console.log("Last Name: " + composer.lastName + "\n" + ", Rating: " + composer.rating + "\n");
+// // console.log("");
 
-// // array like object
-// const obj = { 0: 'a', 1: 'b', 2: 'c' };
-// console.log(Object.entries(obj)); // [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ]
 
-// // array like object with random key ordering
-// const anObj = { 100: 'a', 2: 'b', 7: 'c' };
-// console.log(Object.entries(anObj)); // [ ['2', 'b'], ['7', 'c'], ['100', 'a'] ]
+// // output the composer by genre
 
-// // getFoo is property which isn't enumerable
-// const myObj = Object.create({}, { getFoo: { value() { return this.foo; } } });
-// myObj.foo = 'bar';
-// console.log(Object.entries(myObj)); // [ ['foo', 'bar'] ]
+// // console.log("-- COMPOSER BY GENRE --");
+// // console.log("");
+// // console.log(composersByGenre);
 
-// // non-object argument will be coerced to an object
-// console.log(Object.entries('foo')); // [ ['0', 'f'], ['1', 'o'], ['2', 'o'] ]
 
-// // returns an empty array for any primitive type, since primitives have no own properties
-// console.log(Object.entries(100)); // [ ]
 
-// // iterate through key-value gracefully
-// const obj = { a: 5, b: 7, c: 9 };
-// for (const [key, value] of Object.entries(obj)) {
-//   console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
-// }
+// // end program
 
-// // Or, using array extras
-// Object.entries(obj).forEach(([key, value]) => {
-// console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
-// });
+// // information for looping throught he code follows
+// // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+// // const obj = { foo: 'bar', baz: 42 };
+// // console.log(Object.entries(obj)); // [ ['foo', 'bar'], ['baz', 42] ]
+
+// // // array like object
+// // const obj = { 0: 'a', 1: 'b', 2: 'c' };
+// // console.log(Object.entries(obj)); // [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ]
+
+// // // array like object with random key ordering
+// // const anObj = { 100: 'a', 2: 'b', 7: 'c' };
+// // console.log(Object.entries(anObj)); // [ ['2', 'b'], ['7', 'c'], ['100', 'a'] ]
+
+// // // getFoo is property which isn't enumerable
+// // const myObj = Object.create({}, { getFoo: { value() { return this.foo; } } });
+// // myObj.foo = 'bar';
+// // console.log(Object.entries(myObj)); // [ ['foo', 'bar'] ]
+
+// // // non-object argument will be coerced to an object
+// // console.log(Object.entries('foo')); // [ ['0', 'f'], ['1', 'o'], ['2', 'o'] ]
+
+// // // returns an empty array for any primitive type, since primitives have no own properties
+// // console.log(Object.entries(100)); // [ ]
+
+// // // iterate through key-value gracefully
+// // const obj = { a: 5, b: 7, c: 9 };
+// // for (const [key, value] of Object.entries(obj)) {
+// //   console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+// // }
+
+// // // Or, using array extras
+// // Object.entries(obj).forEach(([key, value]) => {
+// // console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+// // });
 
 
 
